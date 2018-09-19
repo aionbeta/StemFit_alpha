41918b739989907ab1aca5c7d700f80f2ecf349d
aion <ceo@enjoyotto.com>
2018-09-20 00:41:11 +0900

    the virgin commit.

---
+++ app/src/main/java/com/example/aion/alphamemo/IndexActivity.java
@@ -0,0 +1,29 @@
+package com.example.aion.alphamemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class IndexActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goMainActivity();
+            }
+        }, 1500);
+    }
+
+    public void goMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/aion/alphamemo/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.aion.alphamemo;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.aion.alphamemo.adapter.MemoListAdapter;
+import com.example.aion.alphamemo.item.MemoItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+    Context context;
+    RecyclerView memoList;
+    MemoListAdapter memoListAdapter;
+    LinearLayoutManager layoutManager;
+
+    Spinner categorySpinner;
+    EditText memoEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+        setView();
+    }
+
+    private void setView() {
+        categorySpinner = findViewById(R.id.category);
+        memoEdit = findViewById(R.id.memo);
+
+        Button registerButton = findViewById(R.id.register);
+        registerButton.setOnClickListener(this);
+
+        memoList = findViewById(R.id.recyclerview);
+
+        setRecyclerView();
+        setMemoListItem();
+    }
+
+    private void setRecyclerView() {
+        layoutManager = new LinearLayoutManager(context,
+                LinearLayoutManager.VERTICAL, false);
+        memoList.setLayoutManager(layoutManager);
+
+        memoListAdapter = new MemoListAdapter(context,
+                R.layout.row_memo_item, new ArrayList());
+        memoList.setAdapter(memoListAdapter);
+    }
+
+    private void setMemoListItem() {
+        ArrayList<MemoItem> list = getMemoDummyList();
+        memoListAdapter.addItemList(list);
+    }
+
+    private ArrayList<MemoItem> getMemoDummyList() {
+        ArrayList<MemoItem> list = new ArrayList<>();
+
+        MemoItem item1 = new MemoItem("chest",
+                "Bench Press 255lbs 5 5 5 4");
+        MemoItem item2 = new MemoItem("legs",
+                "Low Bar Squats 315lbs 5 5 5 8");
+
+        list.add(item1);
+        list.add(item2);
+        return list;
+    }
+
+    @Override
+    public void onClick(View v) {
+        registerMemo();
+    }
+
+    private void registerMemo() {
+        String category = (String) categorySpinner.getSelectedItem();
+        String memo = memoEdit.getText().toString();
+
+        if (TextUtils.isEmpty(memo)) {
+            Toast.makeText(context, R.string.msg_memo_input, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        addMemoItem(category, memo);
+
+        categorySpinner.setSelection(0);
+        memoEdit.setText("");
+
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm =
+                    (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void addMemoItem(String category, String memo) {
+        MemoItem item = new MemoItem(category, memo);
+
+        memoListAdapter.addItem(item);
+    }
+}
